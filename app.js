bb29759856553e1ac370079de853f52e1b2402c5
Gabriel Felipe de Oliveira <142684077+GabsPere@users.noreply.github.com>
2024-01-24 12:10:48 -0300

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ function gerarNumeroAleatorio() {
         return gerarNumeroAleatorio();
     } else{
         listaDeNumerosSorteados.push(numeroEscolhido);
-        console.log(listaDeNumerosSorteados);
         return numeroEscolhido;
     }
 
